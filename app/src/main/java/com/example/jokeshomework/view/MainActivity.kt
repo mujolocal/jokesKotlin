b99b80c1df6d015b1381d47252389bf7da24108d
muammar johnson <muammarj@gmail.com>
2021-02-10 15:37:06 -0600

    buttons attached and working

--- app/src/main/java/com/example/jokeshomework/view/MainActivity.kt
+++ app/src/main/java/com/example/jokeshomework/view/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.jokeshomework.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.jokeshomework.R
+import com.example.jokeshomework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+       setListeners()
+    }
+    fun setListeners(){
+        binding.requestButton.setOnClickListener {
+            binding.txt1.text = "change is good"
+        }
     }
 }
